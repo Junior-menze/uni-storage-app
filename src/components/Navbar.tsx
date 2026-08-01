28502fb352d4eaac6a84059c90d753dc9d461257
Mvuzo <164266768+Junior-menze@users.noreply.github.com>
2026-08-01 15:29:13 +0200

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -90,7 +90,7 @@ export function Navbar() {
               
               {user ? (
                 <>
-                  <Link href="/dashboard" className="px-3 py-2 text-sm text-muted-foreground hover:text-foreground hover:bg-muted/50 rounded-lg transition">
+                  <Link href={user ? "/booking" : "/auth"}  className="px-3 py-2 text-sm text-muted-foreground hover:text-foreground hover:bg-muted/50 rounded-lg transition">
                     Dashboard
                   </Link>
                   <button
